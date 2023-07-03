34ad483b9a4625b864b2af57fe307994ea16f937
Vetik007 <boy0007@ukr.net>
2023-07-03 19:41:10 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,33 +1,7 @@
-// // import React from 'react';
-// import PropTypes from 'prop-types';
-// import css from './Filter.module.css';
-
-// // Компонент фильтрации контактов
-// function Filter({ value, onChangeFilter }) {
-//   return (
-//     <div className={css.wrapper}>
-//       <label className={css.label}>
-//         Find contacts by name
-//         <input className={css.input} type="text" value={value} onChange={onChangeFilter} />
-//       </label>
-//     </div>
-//   );
-// }
-
-// Filter.propTypes = {
-//   value: PropTypes.string.isRequired,
-//   onChangeFilter: PropTypes.func.isRequired,
-// };
-
-// export default Filter;
-
-// ==========================================
-
 import { useDispatch, useSelector } from 'react-redux';
 import { getContactFilter } from 'redux/selectors';
 import { setContactFilter } from 'redux/filterSlice';
 import css from './Filter.module.css';
-// import { Wrapper, Label, Input } from './Filter.styled';
 
 const Filter = () => {
   const dispatch = useDispatch();
